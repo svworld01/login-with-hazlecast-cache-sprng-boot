84a2bf29034246773453ef6c39138a84bf820b55
svworld01 <saurabh.verma@enveu.com>
2022-01-26 12:42:05 +0530

    adding project

---
+++ src/main/java/com/saurabh/hazelcast/poc/loginwithhazlecastcachesprngboot/LoginWithHazlecastCacheSprngBootApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.hazelcast.poc.loginwithhazlecastcachesprngboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithHazlecastCacheSprngBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithHazlecastCacheSprngBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/hazelcast/poc/loginwithhazlecastcachesprngboot/LoginWithHazlecastCacheSprngBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.hazelcast.poc.loginwithhazlecastcachesprngboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithHazlecastCacheSprngBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
